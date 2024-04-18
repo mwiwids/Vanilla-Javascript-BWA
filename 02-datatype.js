1ef92a1d2dcfbcec52445964d98b6dff661c9c9f
mwiwids <m.wiwid.s@gmail.com>
2024-04-18 23:17:43 +0700

    data type

---
+++ 02-datatype.js
@@ -0,0 +1,8 @@
+// Dynamic Typing : Jenis data type menyesuaikan dengan nilai yang dimasukkan
+
+let value = 12;
+value = "Ice";
+value = true;
+
+console.log(typeof value);
+// number, string, boolean

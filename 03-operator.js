3245942885ac7a0ffa3b4044633fa37e513f46fa
mwiwids <m.wiwid.s@gmail.com>
2024-04-19 20:43:33 +0700

    operator

---
+++ 03-operator.js
@@ -0,0 +1,23 @@
+// aritmatic
+const value = 5 % 3;
+
+// comparison
+const compare = value !== 2;
+
+// unary
+let uny = "5";
+
+console.log(+uny);
+console.log(++uny);
+
+// logical
+const likeJs = true;
+const likeJava = false;
+
+console.log(likeJs && !likeJava);
+console.log(likeJs || likeJava);
+
+// string
+const lang = "JavaScript";
+console.log("Saya sedang belajar " + lang);
+console.log(`Saya sedang belajar ${lang}`);
